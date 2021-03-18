acfcaf761b5255fbc92094a0b8027407cff0df0c
EthanPS23 <ethanps23@gmail.com>
2021-03-18 14:16:45 -0600

    Separated Leaflet styles into separate js file

---
+++ maps/leaflet-styles.js
@@ -0,0 +1,42 @@
+var avyCanadaLayerStyle = {
+    "color": "#ff00ff",
+    "fillcolor": "#000000",
+    "fillOpacity": 0,
+    "weight": 2.5,
+    "opacity": 1
+};
+
+var avyCanadaMCR = L.icon({
+    iconUrl: "https://www.avalanche.ca/static/media/mountain-conditions-report.6aa5980a.svg",
+
+    iconSize: [30,41],
+    iconAnchor: [15,20]
+});
+
+var avyCanadaFatality = L.icon({
+    iconUrl: "https://www.avalanche.ca/static/media/fatal-accident.692404d6.svg",
+
+    iconSize: [30,41],
+    iconAnchor: [15,20]
+});
+
+var avyCanadaWeatherStations = L.icon({
+    iconUrl: "https://www.avalanche.ca/static/media/weather-station.473b0079.svg",
+
+    iconSize: [30,41],
+    iconAnchor: [15,20]
+});
+
+var avyCanadaMINReports = L.icon({
+    iconUrl: "https://www.avalanche.ca/static/media/min-pin.6db47641.svg",
+
+    iconSize: [30,41],
+    iconAnchor: [15,20]
+})
+
+var avyCanadaMINReportsIncident = L.icon({
+    iconUrl: "https://www.avalanche.ca/static/media/min-pin-with-incident.be1c702e.svg",
+
+    iconSize: [30,41],
+    iconAnchor: [15,20]
+})
